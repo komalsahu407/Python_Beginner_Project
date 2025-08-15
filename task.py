23e96cdbbe2b941a255a1d962fae91abb42408e6
komal sahu <komalsahu58590@gmail.com>
2025-08-16 00:08:27 +0530

    day1 completed

---
+++ task.py
@@ -0,0 +1,20 @@
+print("My name is" + " " + "koaml")
+
+#Variables
+name=input("what is your name:")
+print(name)
+
+name="komal"
+print(name)
+print(type(name))
+print(len(name))
+
+name="palak"
+print(name)
+print(type(name))
+print(len(name))
+
+#variable naming
+name = "komal"
+length = len(name)
+print(length)
